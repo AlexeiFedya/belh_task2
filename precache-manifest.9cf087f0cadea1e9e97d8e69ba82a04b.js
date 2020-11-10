fe6b084bb75094e7ccb6311cb4ae43e20e808835
AlexeiFedya <fedya5711@icloud.com>
2020-11-10 23:09:34 +0300

    Updates

---
+++ precache-manifest.9cf087f0cadea1e9e97d8e69ba82a04b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332dc985daf1a04e1eb4170789c96079",
+    "url": "/belh_task2/index.html"
+  },
+  {
+    "revision": "4f4db2764cf099f62d5c",
+    "url": "/belh_task2/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "92a8861e95ae9b29ad2e",
+    "url": "/belh_task2/static/css/main.95640b59.chunk.css"
+  },
+  {
+    "revision": "4f4db2764cf099f62d5c",
+    "url": "/belh_task2/static/js/2.feabb07b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/belh_task2/static/js/2.feabb07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a8861e95ae9b29ad2e",
+    "url": "/belh_task2/static/js/main.b8438d79.chunk.js"
+  },
+  {
+    "revision": "b34504963dda1a0a5951",
+    "url": "/belh_task2/static/js/runtime-main.dcff8c26.js"
+  },
+  {
+    "revision": "0b83231ab475f8770d6f555b4b7de35d",
+    "url": "/belh_task2/static/media/earth_k.0b83231a.png"
+  },
+  {
+    "revision": "b78bca04f1ffb7837c3de10894325471",
+    "url": "/belh_task2/static/media/logo1.b78bca04.svg"
+  }
+]);
